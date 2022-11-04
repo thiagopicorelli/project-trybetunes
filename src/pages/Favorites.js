10e79f6695207032c68411041ba67e879cf65ce9
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-11-04 17:54:55 -0300

    Cria página de favoritos

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,32 +1,72 @@
 import { Component } from 'react';
-import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import { getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 import MusicCard from '../components/MusicCard';
 import Loading from './Loading';
 
 class Favorites extends Component {
-  favoritesPage = (
-    <div></div>
-  );
-
   constructor(props) {
     super(props);
     this.state = {
-      musics: [],
+      musicCards: [],
       isLoading: true,
     };
   }
 
   componentDidMount() {
-    getFavoriteSongs().then((musics) => {
-      console.log(musics);
-    });
+    this.updateMusicElementState();
   }
+
+  musicCardsCreator = (musics) => (
+    musics.map((music, key) => (
+      <MusicCard
+        trackName={ music.trackName }
+        trackId={ parseInt(music.trackId, 10) }
+        previewUrl={ music.previewUrl }
+        favorite
+        onFavoriteChange={ this.onFavoriteChange }
+        key={ key }
+      />
+    ))
+  );
+
+  updateMusicElementState = () => {
+    getFavoriteSongs().then((musics) => {
+      this.setState((prevState) => ({
+        ...prevState,
+        musicCards: this.musicCardsCreator(musics),
+        isLoading: false,
+      }));
+    });
+  };
+
+  onFavoriteChange = (event) => {
+    const id = event.target.getAttribute('id');
+    const song = { trackId: parseInt(id, 10) };
+
+    this.setState((prevState) => ({
+      ...prevState,
+      isLoading: true,
+    }));
+
+    removeSong(song).then(() => {
+      this.updateMusicElementState();
+    });
+  };
+
+  favoritesPage = () => {
+    const { musicCards } = this.state;
+    return (
+      <div>
+        { musicCards }
+      </div>
+    );
+  };
 
   render() {
     const { isLoading } = this.state;
     return (
       <div data-testid="page-favorites">
-        { isLoading ? <Loading /> : this.favoritesPage }
+        { isLoading ? <Loading /> : this.favoritesPage() }
       </div>
     );
   }
